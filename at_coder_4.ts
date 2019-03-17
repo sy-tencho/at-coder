5adaa8ee163313d9435c3c411342bbd1a870fbe2
Shota Yamazaki <sysy5867@gmail.com>
2019-03-17 17:42:01 +0900

    at_coder_4.ts

---
+++ at_coder_4.ts
@@ -0,0 +1,23 @@
+function main(input: string) {
+  const inputNumbers: number[] = input.split('\n').map(n => parseInt(n, 10))
+
+  const A: number = inputNumbers[0]
+  const B: number = inputNumbers[1]
+  const C: number = inputNumbers[2]
+  const X: number = inputNumbers[3]
+
+  let count: number = 0
+
+  for (let i = 0; i <= A; i ++) {
+    for (let j = 0; j <= B; j++) {
+      for (let k = 0; k<= C; k ++) {
+        let result: number = 500 * i + 100 * j + 50 * k
+        if (result === X) count ++ 
+      }
+    }
+  }
+
+  console.log(count)
+}
+
+main(require('fs').readFileSync('/dev/stdin', 'utf8'))
